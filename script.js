39a38d1f5ff175da2ad1411a9a1d8ea996a5dc73
NicolasL <nicolas@transit.app>
2020-05-29 14:57:53 -0400

    streaming popup

--- script.js
+++ script.js
@@ -11,6 +11,10 @@ $(function() {
 	      $('#posterContainer').show();
 	    event.preventDefault();
 	  });
+		$('#openStream').click(function(event) {
+				$('#streamContainer').show();
+			event.preventDefault();
+		});
 	  $('#openAbout').click(function(event) {
 	    if ($('#aboutText').hasClass("grow")) {
 	      $('#aboutText').removeClass("grow").addClass("shrink");
@@ -19,9 +23,16 @@ $(function() {
 	    }
 	  });
 		$('.close').click(function(event) {
+			console.log($(this).parent().attr('id'));
 			$(this).parent().hide();
+			if ($(this).parent().attr('id') == 'streamContainer') {
+				myaudio.pause();
+				$('#streamImage').src = "stm-metro-mono.svg";
+			}
 			event.preventDefault();
 		});
+
+
 		if (screen.width > 1000) {
 		$('.posterRow').click(function(event) {
 			var imageSrc = $(this).find('img').attr('src').replace(/-thumb/, '');
@@ -42,3 +53,32 @@ $(function() {
 		});
 	}
 });
+
+//open stream from VPS
+var myaudio = new Audio('http://rhizotron.net:8080/listen.mp3');
+//determines if audio has been initialized
+let init = false;
+//streaming status
+let muted = true;
+
+//function for index/player play button
+function play() {
+	if (init === false) {
+		myaudio.play();
+		init = true;
+		console.log("stream opened");
+	}
+
+	if (muted === true) {
+		myaudio.muted = false;
+		muted = false;
+		//update to playing icon
+		document.getElementById("streamImage").src = "speaker.svg";
+
+	} else if (muted === false) {
+		myaudio.muted = true;
+		muted = true;
+		//update to muted icon
+		document.getElementById("streamImage").src = "mute.svg";
+	}
+}
