e56e8244dd86381c541a7d28269568b96da39b41
NicolasL <nicolas@transit.app>
2020-05-28 21:24:56 -0400

    mobile friendly

--- script.js
+++ script.js
@@ -1,42 +1,44 @@
 $(function() {
-	$('#openSuscribe').click(function(event) {
-	    $('#subscribeContainer').show();
-		event.preventDefault();
-	});
-  $('#openMixcloud').click(function(event) {
-      $('#mixcloudContainer').show();
-    event.preventDefault();
-  });
-  $('#openPoster').click(function(event) {
-      $('#posterContainer').show();
-    event.preventDefault();
-  });
-  $('#openAbout').click(function(event) {
-    if ($('#aboutText').hasClass("grow")) {
-      $('#aboutText').removeClass("grow").addClass("shrink");
-    } else {
-      $('#aboutText').removeClass('shrink').addClass('grow');
-    }
-  });
-	$('.posterRow').click(function(event) {
-		var imageSrc = $(this).find('img').attr('src').replace(/-thumb/, '');
-		console.log(imageSrc);
-		$('#expandPosterImage').attr('src', imageSrc);
-		$('#expandPosterImage').attr('width', '30%');
-		$('#expandPosterWindow').show();
-	});
-	$('#expandPosterWindow').click(function(event) {
-		$('#expandPosterWindow').hide();
-	});
-	$('.close').click(function(event) {
-		$(this).parent().hide();
-		event.preventDefault();
-	});
+		$('#openSuscribe').click(function(event) {
+		    $('#subscribeContainer').show();
+			event.preventDefault();
+		});
+	  $('#openMixcloud').click(function(event) {
+	      $('#mixcloudContainer').show();
+	    event.preventDefault();
+	  });
+	  $('#openPoster').click(function(event) {
+	      $('#posterContainer').show();
+	    event.preventDefault();
+	  });
+	  $('#openAbout').click(function(event) {
+	    if ($('#aboutText').hasClass("grow")) {
+	      $('#aboutText').removeClass("grow").addClass("shrink");
+	    } else {
+	      $('#aboutText').removeClass('shrink').addClass('grow');
+	    }
+	  });
+		$('.close').click(function(event) {
+			$(this).parent().hide();
+			event.preventDefault();
+		});
+		if (screen.width > 1000) {
+		$('.posterRow').click(function(event) {
+			var imageSrc = $(this).find('img').attr('src').replace(/-thumb/, '');
+			console.log(imageSrc);
+			$('#expandPosterImage').attr('src', imageSrc);
+			$('#expandPosterImage').attr('width', '30%');
+			$('#expandPosterWindow').show();
+		});
+		$('#expandPosterWindow').click(function(event) {
+			$('#expandPosterWindow').hide();
+		});
 
-	$('.draggable').draggable({
-		start: function() {
-		},
-		stop: function() {
-		}
-	});
+		$('.draggable').draggable({
+			start: function() {
+			},
+			stop: function() {
+			}
+		});
+	}
 });
